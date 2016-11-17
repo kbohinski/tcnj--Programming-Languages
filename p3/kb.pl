ba7548b5f4a1c5755208c8071f0e60d564f36180
Kevin Bohinski <kbohinski@gmail.com>
2016-11-17 01:32:38 -0500

    prolog?

--- p3/kb.pl
+++ p3/kb.pl
@@ -12,61 +12,60 @@ cross_river([H, F, LR], [H1, F1, LR1], past_crossings) :-
   cross_river([A, B, C], [H1, F1, LR1], [[A, B, C] | past_crossings]).
 
 % unsafe(NumHens, NumFoxes) determines if it is a safe move...
-unsafe(H, F) :- H < F; H =:= 0.
-safe(H, F) :- not(unsafe(H, F)).
+safe(H, F) :- F =< H; H =:= 0.
 
 % left to right crossings
 cross([H, F, 0], [H2, F2, 1]) :-
   write('One hen crosses L -> R'),
-  H2 =:= (H - 1),
+  H2 is (H - 1),
   H > 0.
 
 cross([H, F, 0], [H2, F2, 1]) :-
   write('Two hens cross L -> R'),
-  H2 =:= (H - 2),
+  H2 is (H - 2),
   H > 1.
 
 cross([H, F, 0], [H2, F2, 1]) :-
   write('One hen and one fox cross L -> R'),
-  H2 =:= (H - 1),
-  F2 =:= (F - 1),
+  H2 is (H - 1),
+  F2 is (F - 1),
   H > 0,
   F > 0.
 
 cross([H, F, 0], [H2, F2, 1]) :-
   write('One fox crosses L -> R'),
-  F2 =:= (F - 1),
+  F2 is (F - 1),
   F > 0.
 
 cross([H, F, 0], [H2, F2, 1]) :-
   write('Two foxes cross L -> R'),
-  F2 =:= (F - 2),
+  F2 is (F - 2),
   F > 1.
 
 % right to left crossings
 cross([H, F, 1], [H2, F2, 0]) :-
   write('One hen crosses L <- R'),
-  H2 =:= (H + 1),
+  H2 is (H + 1),
   H < 3.
 
 cross([H, F, 1], [H2, F2, 0]) :-
   write('Two hens cross L <- R'),
-  H2 =:= (H + 2),
+  H2 is (H + 2),
   H < 2.
 
 cross([H, F, 1], [H2, F2, 0]) :-
   write('One hen and one fox crosses L <- R'),
-  F2 =:= F + 1,
-  H2 =:= H + 1,
+  F2 is F + 1,
+  H2 is H + 1,
   F < 3,
   H < 3.
 
 cross([H, F, 1], [H2, F2, 0]) :-
   write('One fox crosses L <- R'),
-  F2 =:= (F + 1),
+  F2 is (F + 1),
   F < 3.
 
 cross([H, F, 1], [H2, F2, 0]) :-
   write('Two foxes cross L <- R'),
-  F2 =:= (F + 2),
+  F2 is (F + 2),
   F < 2.
