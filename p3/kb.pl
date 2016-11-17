ba8f43ea7039834185e5120d3f0433f1f7b796d3
Kevin Bohinski <kbohinski@gmail.com>
2016-11-17 00:17:17 -0500

    prolog?2

--- p3/kb.pl
+++ p3/kb.pl
@@ -1,5 +1,5 @@
 % unsafe(NumHens, NumFoxes) determines if it is a safe move...
-unsafe(H, F) :- H =< F.
+unsafe(H, F) :- H < F; H =:= 0.
 safe(H, F) :- not(unsafe(H, F)).
 
 % left to right crossings
@@ -27,7 +27,23 @@ cross([H, F, 0], [H2, F2, 1]) :-
 
 % right to left crossings
 cross([H, F, 1], [H2, F2, 0]) :-
+  H2 =:= (H + 1),
+  H < 3.
+
 cross([H, F, 1], [H2, F2, 0]) :-
+  H2 =:= (H + 2),
+  H < 2.
+
 cross([H, F, 1], [H2, F2, 0]) :-
+  F2 =:= F + 1,
+  H2 =:= H + 1,
+  F < 3,
+  H < 3.
+
 cross([H, F, 1], [H2, F2, 0]) :-
+  F2 =:= (F + 1),
+  F < 3.
+
 cross([H, F, 1], [H2, F2, 0]) :-
+  F2 =:= (F + 2),
+  F < 2.
