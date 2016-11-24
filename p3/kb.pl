d330c0042bc8acd1a8ad3faabbe034d93c5c8aae
Kevin Bohinski <kbohinski@gmail.com>
2016-11-23 21:03:35 -0500

    plz wrk

--- p3/kb.pl
+++ p3/kb.pl
@@ -8,17 +8,17 @@
 % kb.pl
 
 % Module to ensure math is integer...
-:- use_module(library(clpfd)).
+% :- use_module(library(clpfd)).
 
 % Overarching functions to solve riddle
 solve :-
   start_state(X),
   printl(X),
-  solve(X, [X]).
+  solve(X, [X], 0).
 
-solve(S, Crossings) :-
+solve(S, Crossings, Last) :-
   end_state(S);
-  (cross(S, N), not(member(N, Crossings)), printl(Crossings), solve(N, [N, Crossings])).
+  (cross(S, N, Last, K), not(member(N, Crossings)), solve(N, [N | Crossings], K)).
 
 % DOES NOT WORK -- Was a BFS to determine number of possible solutions per spec.
 % solve_bfs :-
@@ -46,8 +46,8 @@ solve(S, Crossings) :-
 %   end_state(State).
 
 % Riddle start and end states
-start_state([3, 3, 0]).
-end_state([0, 0, 1]).
+start_state([3, 3, 1, 0, 0]).
+end_state([0, 0, 0, 0, 0]).
 
 % Helper functions
 printl(String) :- write(String), nl.
@@ -57,73 +57,94 @@ print_trip_left(S, N) :- write(N), write(' <- '), write(S), nl.
 % All possible river crossings rules, one hen crosses are removed as they will not occur...
 
 % Left to right crossings
-cross([H1, F1, 0], [H2, F2, 1]) :-
-  % F2 =:= F1,
-  H2 #= H1 - 2,
-  H1 > 1,
-  safe(H2, F1),
-  printl('Two hens cross L -> R'),
-  print_trip_right([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 1, HR, FR], [HLN, FL, 0, HRN, FR], Last, Move) :-
+  not(Last == 1),
+  Move is 1,
+  HL >= 2,
+  HLN is HL - 2,
+  HRN is HR + 2,
+  safe([HLN, FL, 0, HRN, FR]),
+  print_trip_right([HL, FL, 1, HR, FR], [HLN, FL, 0, HRN, FR]),
+  printl('Two hens cross L -> R').
 
-cross([H1, F1, 0], [H2, F2, 1]) :-
-  H2 #= H1 - 1,
-  F2 #= F1 - 1,
-  H1 > 0,
-  F1 > 0,
-  safe(H2, F2),
-  printl('One hen and one fox cross L -> R'),
-  print_trip_right([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 1, HR, FR], [HLN, FLN, 0, HRN, FRN], Last, Move) :-
+  not(Last == 2),
+  Move is 2,
+  HL >= 1,
+  FL >= 1,
+  HRN is HR + 1,
+  FRN is FR + 1,
+  HLN is HL - 1,
+  FLN is FL - 1,
+  safe([HLN, FLN, 0, HRN, FRN]),
+  print_trip_right([HL, FL, 1, HR, FR], [HLN, FLN, 0, HRN, FRN]),
+  printl('One hen and one fox cross L -> R').
 
-cross([H1, F1, 0], [H2, F2, 1]) :-
-  % H1 =:= H2,
-  F2 #= F1 - 1,
-  F1 > 0,
-  safe(H1, F2),
-  printl('One fox cross L -> R'),
-  print_trip_right([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 1, HR, FR], [HL, FLN, 0, HR, FRN], Last, Move) :-
+  not(Last == 3),
+  Move is 3,
+  FL >= 1,
+  FLN is FL - 1,
+  FRN is FR + 1,
+  safe([HL, FLN, 0, HR, FRN]),
+  print_trip_right([HL, FL, 1, HR, FR], [HL, FL, 0, HR, FR]),
+  printl('One fox cross L -> R').
 
-cross([H1, F1, 0], [H2, F2, 1]) :-
-  % H1 =:= H2,
-  F2 #= F1 - 2,
-  F1 > 1,
-  safe(H1, F2),
-  printl('Two foxes cross L -> R'),
-  print_trip_right([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 1, HR, FR], [HL, FLN, 0, HR, FRN], Last, Move) :-
+  not(Last == 4),
+  Move is 4,
+  FL >= 2,
+  FLN is FL - 2,
+  FRN is FR + 2,
+  safe([HL, FLN, 0, HR, FRN]),
+  print_trip_right([HL, FL, 1, HR, FR], [HL, FL, 0, HR, FR]),
+  printl('Two foxes cross L -> R').
 
 % Right to left crossings
-cross([H1, F1, 1], [H2, F2, 0]) :-
-  % F2 =:= F1,
-  H2 #= H1 + 2,
-  H1 < 2,
-  safe(H2, F1),
-  printl('Two hens cross L <- R'),
-  print_trip_left([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 0, HR, FR], [HLN, FL, 1, HRN, FR], Last, Move) :-
+  not(Last == 1),
+  Move is 1,
+  HR >= 2,
+  HLN is HL + 2,
+  HRN is HR - 2,
+  safe([HLN, FL, 1, HRN, FR]),
+  print_trip_left([HL, FL, 0, HR, FR], [HLN, FL, 1, HRN, FR]),
+  printl('Two hens cross L <- R').
 
-cross([H1, F1, 1], [H2, F2, 0]) :-
-  F2 #= F1 + 1,
-  H2 #= H1 + 1,
-  F1 < 3,
-  H1 < 3,
-  safe(H2, F2),
-  printl('One hen and one fox cross L <- R'),
-  print_trip_left([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 0, HR, FR], [HLN, FLN, 1, HRN, FRN], Last, Move) :-
+  not(Last == 2),
+  Move is 2,
+  HR >= 1,
+  FR >= 1,
+  HRN is HR - 1,
+  FRN is FR - 1,
+  HLN is HL + 1,
+  FLN is FL + 1,
+  safe([HLN, FLN, 0, HRN, FRN]),
+  print_trip_left([HL, FL, 0, HR, FR], [HLN, FLN, 1, HRN, FRN]),
+  printl('One hen and one fox cross L <- R').
 
-cross([H1, F1, 1], [H2, F2, 0]) :-
-  % H1 =:= H2,
-  F2 #= F1 + 1,
-  F1 < 3,
-  safe(H1, F2),
-  printl('One fox cross L <- R'),
-  print_trip_left([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 0, HR, FR], [HL, FLN, 1, HR, FRN], Last, Move) :-
+  not(Last == 3),
+  Move is 3,
+  FR >= 1,
+  FRN is FR - 1,
+  FLN is FL + 1,
+  safe([HL, FLN, 1, HR, FRN]),
+  print_trip_left([HL, FL, 0, HR, FR], [HL, FLN, 1, HR, FRN]),
+  printl('One fox cross L <- R').
 
-cross([H1, F1, 1], [H2, F2, 0]) :-
-  % H1 =:= H2,
-  F2 #= F1 + 2,
-  F1 < 2,
-  safe(H1, F2),
-  printl('Two foxes cross L <- R'),
-  print_trip_left([H1, F1, 0], [H2, F2, 1]).
+cross([HL, FL, 0, HR, FR], [HL, FLN, 1, HR, FRN], Last, Move) :-
+  not(Last == 4),
+  Move is 4,
+  FR >= 2,
+  FRN is FR - 2,
+  FLN is FL + 2,
+  safe([HL, FLN, 1, HR, FRN]),
+  print_trip_left([HL, FL, 0, HR, FR], [HL, FLN, 1, HR, FRN]),
+  printl('Two foxes cross L <- R').
 
 % Safe function to make sure hens dont get eaten.
-safe(H, F) :- 
-  (F =< H; H =:= 0).
+safe([HL, FL, _, HR, FR]) :- 
+  HL >= FL,
+  HR >= FR.
